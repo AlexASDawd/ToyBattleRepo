1ffd0eceb2078ea71ce3470b9d69f6d2732d606e
AlexASDawd <AlexMurphy841@gmail.com>
2024-10-01 19:20:01 +0400

    Adding sounds

--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -8,6 +8,7 @@ public class SceneHandler : MonoBehaviour
     [SerializeField] private List<ToyGroupHandler> _toyGroups;
     [SerializeField] private Fader _fader;
     [SerializeField] private int _sceneToLoad = 1;
+    [SerializeField] private AudioSource _winSound;
     public bool _isMenuScene = false;
     private bool _gameIsEnded = false;
     public void LoadSceneHandler() {
@@ -24,6 +25,8 @@ public class SceneHandler : MonoBehaviour
 
     public void EndGame(int sceneIndex) {
         if (!_gameIsEnded) {
+
+            _winSound.Play();
             Debug.Log("LoadingScene");
             _sceneToLoad = sceneIndex;
             _gameIsEnded = true;
